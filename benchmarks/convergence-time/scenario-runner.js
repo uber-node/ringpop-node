49d24fe67fc1d9d8afa252de26a6b3770f5b3d31
Daniel Simmons <dans@uber.com>
2016-04-14 10:38:07 +0200

    Update licence headers

--- benchmarks/convergence-time/scenario-runner.js
+++ benchmarks/convergence-time/scenario-runner.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Copyright (c) 2015 Uber Technologies, Inc.
+// Copyright (c) 2016 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 'use strict';
 var assert = require('assert');
 var cp = require('child_process');
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Uber Technologies, Inc.
+// Copyright (c) 2016 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -17,4 +17,5 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 require('glob').sync(__dirname + '/**/*{_test,-test}.js').forEach(require);
