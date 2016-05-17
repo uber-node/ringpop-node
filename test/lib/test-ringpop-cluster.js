d74618bd3bb0c4b084ebc556048fcd4df211ea61
Menno Pruijssers <menno@wyzers.com>
2016-05-17 14:23:04 +0200

    Revert "Partition healing (#264) and "Periodical partition healing (#265)" (#270)
    6c025bcc67bdeb1dd5b55269195a740f2e283fc5a and 02be5362b835be17c70f9af5fe418f230cf07af1 are using a dev-dependency (async) from production code. This commit those two commits so master is in a stable state again.

--- test/lib/test-ringpop-cluster.js
+++ test/lib/test-ringpop-cluster.js
@@ -167,7 +167,7 @@ function testRingpopCluster(opts, name, test) {
         if (opts.waitForConvergence !== false) {
             var onOneExhausted = _.after(cluster.length, onSteadyState);
             cluster.forEach(function each(ringpop) {
-                ringpop.dissemination.once('changesExhausted', onOneExhausted);
+                ringpop.dissemination.on('changesExhausted', onOneExhausted);
             });
         }
 
