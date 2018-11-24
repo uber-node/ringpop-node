2234f45ac82c1a0d63220303ec374930d1decd9f
Eric Satterwhite <esatterwhite@wi.rr.com>
2018-11-23 21:50:54 -0500

    deps: farmhash@2.1.0

--- index.js
+++ index.js
@@ -136,10 +136,8 @@ function RingPop(options) {
     // use fingerprint if ringpop needs to function cross different platforms
     if (this.config.get('isCrossPlatform')) {
         this.hashFunc = farmhash.fingerprint32;
-    } else if (this.config.get('useLatestHash32')) {
-        this.hashFunc = farmhash.hash32;
     } else {
-        this.hashFunc = farmhash.hash32v1;
+        this.hashFunc = farmhash.hash32;
     }
 
     this.lagSampler = new LagSampler({
